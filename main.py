d3fb85d120dae49dc12d19aebb6b791b726f6c1c
Prem <premprakashjena04@gmail.com>
2026-03-12 03:12:20 +0530

    feat: integrate PostgreSQL with SQLAlchemy and create users table

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from routers import flights, bookings
+from database import engine
+from models import user
+from database import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
